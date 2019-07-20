caebb7ad953a4048a785fd71b6b52c0ad43f7d34
HoangAnh <hoanganh.hn0301@gmail.com>
2019-07-21 00:05:37 +0700

    [1.0.0] define model Schema

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+let app = express();
+
+let hostname = "localhost";
+let port = 8017;
+
+app.get("/helloworld" ,(req,res) => {
+  res.send("<h1>Hello world !!! </h1>")
+});
+
+app.listen(port, hostname, ()=>{
+  console.log('Listen in port : ', port);
+})
